91aeafb072de6da7bf007a65b11bd764f106685e
RodrigoAMM <medina.rodrigo@csmstudents.org>
2019-05-17 15:57:57 +0000

    took out print

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,4 @@
-a="That is really good!" 
+a="That is really good!"
 b="I hope you get better"
 c="oh, ok"
 name=input(("Hello there, what is your name?"))
@@ -23,9 +23,9 @@ print("Do you like my Website?")
 print(website)
 if(website=="yes"):
     print("Thank you")
-    
-print("Would you like to invest in it?")  
-#this print is the same as the variable, but it prints the question 
+
+print("Would you like to invest in it?")
+#this print is the same as the variable, but it prints the question
 if(invest=="yes"):
     print("yes, $"+str(money))
     print("thank you very much!")
